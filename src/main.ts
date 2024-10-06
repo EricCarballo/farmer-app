1c3095d3239f230f81e122f0d93e4b8d5595968d
Eruviel Garcia <eruvielgs@gmail.com>
2024-10-06 10:46:30 -0600

    CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle("Farmer's app")
     .setDescription('The farmer API description')
